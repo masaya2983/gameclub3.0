751a1cdfdf5de7dc299d3885f7247a95b1fd895f
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-06-10 12:18:34 +0000

    やり直し

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,4 @@
-class Comment < ApplicationRecord
+class Favorite < ApplicationRecord
   belongs_to :member
   belongs_to :game
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_10_121323) do
+ActiveRecord::Schema.define(version: 2023_06_10_121600) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -58,6 +58,11 @@ ActiveRecord::Schema.define(version: 2023_06_10_121323) do
   end
 
   create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
