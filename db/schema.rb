1cb7fa24d76042d82b87c2b2f5fd0ad04cd60634
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-06-10 12:10:33 +0000

    やり直し

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_10_120120) do
+ActiveRecord::Schema.define(version: 2023_06_10_121012) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -50,6 +50,16 @@ ActiveRecord::Schema.define(version: 2023_06_10_120120) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_adomins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_adomins_on_reset_password_token", unique: true
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "members", force: :cascade do |t|
