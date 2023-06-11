c688841b135fd1ec98aab8f5fa0e7eb8c00cc285
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-06-11 03:04:20 +0000

    やり直し

--- app/controllers/public/games_controller.rb
+++ app/controllers/public/games_controller.rb
@@ -2,15 +2,22 @@ class Public::GamesController < ApplicationController
   
   def show
    @gane = Game.find(params[:id])
-   
+   @comment = Commemt.new
   end
   
   def index
- 
+  @games = Game.all
+  @game = Game.new
   end
  
  def create
-   
+  @game = Game.new(game_params)
+  @game.member_id = current_member.id
+  if @game.save
+  else
+  
+  end
+      
  end
  
  def edit
@@ -18,16 +25,20 @@ class Public::GamesController < ApplicationController
  end
  
  def update
+   if @game.update(game_params)
+   else
    
+   end
  end 
  
  def destroy
- 
+  @game.destroy
  end
  
  private
  
  def game_params
+     params.require(:game).permit(:tittle, :body)
  end
  
 end
