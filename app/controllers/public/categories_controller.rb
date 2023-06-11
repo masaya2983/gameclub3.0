3b0103b8dcda3957d3ba3994936b69e2afb86966
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-06-11 06:09:10 +0000

    やり直し

--- app/controllers/public/categories_controller.rb
+++ app/controllers/public/categories_controller.rb
@@ -1,2 +1,3 @@
 class Public::CategoriesController < ApplicationController
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,23 @@ Rails.application.routes.draw do
     registrstions: "publick/ registrstions",
     sessions: 'publick/sessions'
   }
-  
+  namespace :admin do
+   root :to => "homes#top"
+   resources :members, only: [:index,:show,:edit,:update]
+   resources :games, only: [:index,:show,:edit,:update]
+   resources :category, only: [:index,:show,:edit,:update]
+ end
+ scope module: :public do
+   root :to =>"homes#top"
+   get "about" => "homes#top"
+   resources :member, only: [:index,:show,:edit,:update]
+   resource :relationships, only: [:create, :destroy]
+   get 'followings' => 'relationships#followings', as: 'followings'
+   get 'followers' => 'relationships#followers', as: 'followers'
+   resources :games, only: [:index,:create,:show,:update,:destroy]
+   resources :coments, only: [:create,:destroy]
+   resources :category, only: [:index,:create,:show,:update]
+   resource :favorites, only:[:create,:destroy]
+ end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
